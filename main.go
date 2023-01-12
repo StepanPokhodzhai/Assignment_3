101f1a24294f271ff70e06c241cf65c24d7616ca
StepanPokhodzhai <stelkerstepan@gmail.com>
2023-01-12 17:38:33 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	//fmt.Println(os.Args[1])
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(404)
+	}
+
+	io.Copy(os.Stdout, f)
+}
